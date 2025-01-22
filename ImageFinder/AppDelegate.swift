9e62f22f951baaa9f6a0bd4bfa8f8ef510d227a2
조다은 <thisisdana49@gmail.com>
2025-01-22 20:20:13 +0900

    refactor: UIAppearance 적용 #11

--- ImageFinder/AppDelegate.swift
+++ ImageFinder/AppDelegate.swift
@@ -13,7 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // UIAppearance
+        UIView.appearance().backgroundColor = .white
+        UINavigationBar.appearance().tintColor = .black
+        
         return true
     }
 
